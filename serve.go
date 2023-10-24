6a97ce07c890c69800c4c4fdd382f9b90dc06533
Hugo Borsoni <hugobo@theodo.fr>
2023-10-24 10:13:19 +0200

    refacto: be clever and use a go routine at function call rather than in function

--- serve.go
+++ serve.go
@@ -7,7 +7,7 @@ import (
 )
 
 func (s *Server) Run() {
-	s.GenerateOpenAPI()
+	go s.GenerateOpenAPI()
 	elapsed := time.Since(s.startTime)
 	slog.Debug("Server started in "+elapsed.String(), "info", "time between since server creation (op.NewServer) and server startup (op.Run). Depending on your implementation, there might be things that do not depend on op slowing start time")
 	slog.Info("Server running ✅ on http://localhost"+s.Addr, "started in", elapsed.String())
