aa82ec6361e2a32e423a2829093cfcffb1a445dd
Siddhartha Khuntia <77291662+sidkhuntia@users.noreply.github.com>
2023-10-25 21:43:46 +0530

    Handles all HTTP methods along with std version (#14)
    * added other http methods along with std version
    
    * added tests for http methods

--- mux_test.go
+++ mux_test.go
@@ -36,3 +36,163 @@ func TestUseStd(t *testing.T) {
 	require.Equal(t, w.Code, http.StatusOK)
 	require.Equal(t, w.Body.String(), "test successful")
 }
+
+func TestGet(t *testing.T) {
+	s := NewServer()
+	Get(s, "/test", func(ctx Ctx[string]) (string, error) {
+		return "test", nil
+	})
+
+	r := httptest.NewRequest(http.MethodGet, "/test", nil)
+	w := httptest.NewRecorder()
+
+	s.mux.ServeHTTP(w, r)
+
+	require.Equal(t, w.Code, http.StatusOK)
+	require.Equal(t, w.Body.String(), "\"test\"\n")
+}
+
+func TestPost(t *testing.T) {
+	s := NewServer()
+	Post(s, "/test", func(ctx Ctx[string]) (string, error) {
+		return "test", nil
+	})
+
+	r := httptest.NewRequest(http.MethodPost, "/test", nil)
+	w := httptest.NewRecorder()
+
+	s.mux.ServeHTTP(w, r)
+
+	require.Equal(t, w.Code, http.StatusOK)
+	require.Equal(t, w.Body.String(), "\"test\"\n")
+}
+
+func TestPut(t *testing.T) {
+	s := NewServer()
+	Put(s, "/test", func(ctx Ctx[string]) (string, error) {
+		return "test", nil
+	})
+
+	r := httptest.NewRequest(http.MethodPut, "/test", nil)
+	w := httptest.NewRecorder()
+
+	s.mux.ServeHTTP(w, r)
+
+	require.Equal(t, w.Code, http.StatusOK)
+	require.Equal(t, w.Body.String(), "\"test\"\n")
+}
+
+func TestPatch(t *testing.T) {
+	s := NewServer()
+	Patch(s, "/test", func(ctx Ctx[string]) (string, error) {
+		return "test", nil
+	})
+
+	r := httptest.NewRequest(http.MethodPatch, "/test", nil)
+	w := httptest.NewRecorder()
+
+	s.mux.ServeHTTP(w, r)
+
+	require.Equal(t, w.Code, http.StatusOK)
+	require.Equal(t, w.Body.String(), "\"test\"\n")
+}
+
+func TestDelete(t *testing.T) {
+	s := NewServer()
+	Delete(s, "/test", func(ctx Ctx[string]) (string, error) {
+		return "test", nil
+	})
+
+	r := httptest.NewRequest(http.MethodDelete, "/test", nil)
+	w := httptest.NewRecorder()
+
+	s.mux.ServeHTTP(w, r)
+
+	require.Equal(t, w.Code, http.StatusOK)
+	require.Equal(t, w.Body.String(), "\"test\"\n")
+}
+
+func TestGetStd(t *testing.T) {
+	s := NewServer()
+	GetStd(s, "/test", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("test successful"))
+	})
+
+	r := httptest.NewRequest(http.MethodGet, "/test", nil)
+
+	w := httptest.NewRecorder()
+
+	s.mux.ServeHTTP(w, r)
+
+	require.Equal(t, w.Code, http.StatusOK)
+	require.Equal(t, w.Body.String(), "test successful")
+}
+
+func TestPostStd(t *testing.T) {
+	s := NewServer()
+	PostStd(s, "/test", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("test successful"))
+	})
+
+	r := httptest.NewRequest(http.MethodPost, "/test", nil)
+
+	w := httptest.NewRecorder()
+
+	s.mux.ServeHTTP(w, r)
+
+	require.Equal(t, w.Code, http.StatusOK)
+	require.Equal(t, w.Body.String(), "test successful")
+}
+
+func TestPutStd(t *testing.T) {
+	s := NewServer()
+	PutStd(s, "/test", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("test successful"))
+	})
+
+	r := httptest.NewRequest(http.MethodPut, "/test", nil)
+
+	w := httptest.NewRecorder()
+
+	s.mux.ServeHTTP(w, r)
+
+	require.Equal(t, w.Code, http.StatusOK)
+	require.Equal(t, w.Body.String(), "test successful")
+}
+
+func TestPatchStd(t *testing.T) {
+	s := NewServer()
+	PatchStd(s, "/test", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("test successful"))
+	})
+
+	r := httptest.NewRequest(http.MethodPatch, "/test", nil)
+
+	w := httptest.NewRecorder()
+
+	s.mux.ServeHTTP(w, r)
+
+	require.Equal(t, w.Code, http.StatusOK)
+	require.Equal(t, w.Body.String(), "test successful")
+}
+
+func TestDeleteStd(t *testing.T) {
+	s := NewServer()
+	DeleteStd(s, "/test", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("test successful"))
+	})
+
+	r := httptest.NewRequest(http.MethodDelete, "/test", nil)
+
+	w := httptest.NewRecorder()
+
+	s.mux.ServeHTTP(w, r)
+
+	require.Equal(t, w.Code, http.StatusOK)
+	require.Equal(t, w.Body.String(), "test successful")
+}
