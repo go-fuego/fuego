e95b9c0f78faae51de8c03fec894114e4d1418e6
EwenQuim <ewen.quimerch@gmail.com>
2023-11-01 18:34:31 +0100

    Cleaner example

--- examples/simple-crud/controller/routes.go
+++ examples/simple-crud/controller/routes.go
@@ -1,21 +1,28 @@
 package controller
 
 import (
-	"fmt"
+	"time"
 
 	"simple-crud/store"
 
 	"github.com/go-op/op"
 )
 
+// Ressource is the struct that holds useful sources of informations available for the controllers.
 func NewRessource(queries store.Queries) Ressource {
 	return Ressource{
 		Queries: queries,
 	}
 }
 
+// Ressource is the struct that holds useful sources of informations available for the controllers.
 type Ressource struct {
-	Queries store.Queries
+	Queries     store.Queries          // Database queries
+	UserQueries store.Queries          // Database queries from another store
+	ExternalAPI interface{}            // External API
+	Cache       map[string]interface{} // Some cache
+	Now         func() time.Time       // Function to get the current time
+
 }
 
 func (rs Ressource) Routes(s *op.Server) {
@@ -28,13 +35,6 @@ func (rs Ressource) Routes(s *op.Server) {
 		WithDescription("Get all recipes").
 		AddTags("custom")
 
-	for i := 0; i < 10; i++ {
-		op.Get(s, fmt.Sprintf("/recipe/%d", i), rs.getAllRecipes).
-			WithSummary("Get recipe").
-			WithDescription("Get recipe").
-			SetTags("custom")
-	}
-
 	op.Post(s, "/recipes/new", rs.newRecipe)
 
 	op.Get(s, "/recipes/{id}", rs.getRecipeWithIngredients)
@@ -43,4 +43,20 @@ func (rs Ressource) Routes(s *op.Server) {
 	op.Post(s, "/ingredients/new", rs.newIngredient)
 
 	op.Post(s, "/dosings/new", rs.newDosing)
+
+	op.Group(s, "/api", func(s *op.Server) {
+		op.Get(s, "/mounted-route", func(c op.Ctx[any]) (string, error) {
+			return "hello", nil
+		})
+
+		op.Post(s, "/mounted-route-post", func(c op.Ctx[any]) (string, error) {
+			return "hello", nil
+		})
+
+		op.Group(s, "/mounted-group", func(groupedS *op.Server) {
+			op.Get(groupedS, "/mounted-route", func(c op.Ctx[any]) (string, error) {
+				return "hello", nil
+			})
+		})
+	})
 }
