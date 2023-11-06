a51f1cd82668973a5a017e8648f661b4a27f5285
EwenQuim <ewen.quimerch@gmail.com>
2023-11-06 12:07:03 +0100

    Basic auth middleware

---
+++ middleware/basicauth/basicauth.go
@@ -0,0 +1,41 @@
+package basicauth
+
+import (
+	"net/http"
+
+	"github.com/go-op/op"
+)
+
+type Config struct {
+	Username string
+	Password string
+}
+
+// Basic auth middleware
+func New(config Config) func(http.Handler) http.Handler {
+	if config.Username == "" {
+		panic("basicauth: username is required")
+	}
+	if config.Password == "" {
+		panic("basicauth: password is required")
+	}
+
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			user, pass, ok := r.BasicAuth()
+
+			if ok && user == config.Username && pass == config.Password {
+				h.ServeHTTP(w, r)
+				return
+			}
+
+			err := op.ErrorResponse{
+				Message:    "unauthorized",
+				StatusCode: http.StatusUnauthorized,
+			}
+
+			w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+			op.SendJSONError(w, err)
+		})
+	}
+}
