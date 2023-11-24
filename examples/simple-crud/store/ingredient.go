b7db1485ae352db4420661c7e19967be2ffd32cf
EwenQuim <ewen.quimerch@gmail.com>
2023-11-24 15:08:36 +0100

    Better navigation & CSS

---
+++ examples/simple-crud/store/ingredient.go
@@ -0,0 +1,17 @@
+package store
+
+import (
+	"strings"
+
+	"github.com/go-fuego/fuego"
+	"github.com/google/uuid"
+)
+
+var _ fuego.InTransformer = (*CreateIngredientParams)(nil)
+
+func (c *CreateIngredientParams) InTransform() error {
+	c.ID = uuid.NewString()
+	c.Name = strings.TrimSpace(c.Name)
+
+	return nil
+}
