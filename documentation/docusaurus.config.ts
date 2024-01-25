c7864b7342f22a0696c79e548ed598e7ed1d84db
EwenQuim <ewen.quimerch@gmail.com>
2024-01-25 09:24:46 +0100

    docs: Fixes docusaurus baseURL to /fuego

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: "https://go-fuego.github.io", //TODO
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/fuego",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
