3c5139391fcf60a9b0b195b2e47eb3e9fcb786ee
EwenQuim <ewen.quimerch@gmail.com>
2024-03-21 02:05:00 +0100

    Errors: added more tests

--- errors_test.go
+++ errors_test.go
@@ -28,9 +28,12 @@ func TestErrorHandler(t *testing.T) {
 	})
 
 	t.Run("not found error", func(t *testing.T) {
-		err := NotFoundError{}
+		err := NotFoundError{
+			Err: errors.New("Not Found :c"),
+		}
 		errResponse := ErrorHandler(err)
 		require.ErrorAs(t, errResponse, &HTTPError{})
+		require.Contains(t, err.Error(), "Not Found :c")
 		require.Contains(t, errResponse.Error(), "Not Found")
 		require.Contains(t, errResponse.Error(), "404")
 		require.Equal(t, http.StatusNotFound, errResponse.(HTTPError).StatusCode())
@@ -46,4 +49,71 @@ func TestErrorHandler(t *testing.T) {
 		require.Contains(t, errResponse.Error(), "404")
 		require.Equal(t, http.StatusNotFound, errResponse.(HTTPError).StatusCode())
 	})
+
+	t.Run("conflict error", func(t *testing.T) {
+		err := ConflictError{
+			Err: errors.New("Conflict"),
+		}
+		errResponse := ErrorHandler(err)
+		require.ErrorAs(t, errResponse, &HTTPError{})
+		require.Contains(t, err.Error(), "Conflict")
+		require.Contains(t, errResponse.Error(), "Conflict")
+		require.Contains(t, errResponse.Error(), "409")
+		require.Equal(t, http.StatusConflict, errResponse.(HTTPError).StatusCode())
+	})
+
+	t.Run("unauthorized error", func(t *testing.T) {
+		err := UnauthorizedError{
+			Err: errors.New("coucou"),
+		}
+		errResponse := ErrorHandler(err)
+		require.ErrorAs(t, errResponse, &HTTPError{})
+		require.Contains(t, err.Error(), "coucou")
+		require.Contains(t, errResponse.Error(), "Unauthorized")
+		require.Contains(t, errResponse.Error(), "401")
+		require.Equal(t, http.StatusUnauthorized, errResponse.(HTTPError).StatusCode())
+	})
+
+	t.Run("forbidden error", func(t *testing.T) {
+		err := ForbiddenError{
+			Err: errors.New("Forbidden"),
+		}
+		errResponse := ErrorHandler(err)
+		require.ErrorAs(t, errResponse, &HTTPError{})
+		require.Contains(t, err.Error(), "Forbidden")
+		require.Contains(t, errResponse.Error(), "Forbidden")
+		require.Contains(t, errResponse.Error(), "403")
+		require.Equal(t, http.StatusForbidden, errResponse.(HTTPError).StatusCode())
+	})
+}
+
+func TestHTTPError_Error(t *testing.T) {
+	t.Run("title", func(t *testing.T) {
+		t.Run("custom title", func(t *testing.T) {
+			err := HTTPError{
+				Title: "Custom Title",
+			}
+			require.Contains(t, err.Error(), "Custom Title")
+		})
+		t.Run("title from status", func(t *testing.T) {
+			err := HTTPError{Status: http.StatusNotFound}
+			require.Contains(t, err.Error(), "Not Found")
+		})
+		t.Run("default title", func(t *testing.T) {
+			err := HTTPError{}
+			require.Contains(t, err.Error(), "HTTP Error")
+		})
+	})
+}
+
+func TestHTTPError_Unwrap(t *testing.T) {
+	err := myError{status: 999}
+
+	errResponse := HTTPError{
+		Err: err,
+	}
+
+	var unwrapped myError
+	require.True(t, errors.As(errResponse.Unwrap(), &unwrapped))
+	require.Equal(t, 999, unwrapped.status)
 }
