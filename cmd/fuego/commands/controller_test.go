3fb20b3ae1045dbacc7127bf38f560f7b0d84c4c
EwenQuim <ewen.quimerch@gmail.com>
2024-03-22 14:04:38 +0100

    Send raw error

--- cmd/fuego/commands/controller_test.go
+++ cmd/fuego/commands/controller_test.go
@@ -1,15 +1,18 @@
 package commands
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestCreateController(t *testing.T) {
-	res, err := createController("books", "/dev/null")
+	res, err := createController("books")
 	require.NoError(t, err)
 	require.Contains(t, res, "package controller")
 	require.Contains(t, res, `fuego.Get(booksGroup, "/{id}", rs.getBooks)`)
 	require.Contains(t, res, `func (rs BooksRessources) postBooks(c *fuego.ContextWithBody[BooksCreate]) (Books, error)`)
+	require.FileExists(t, "./controllers/books.go")
+	os.Remove("./controllers/books.go")
 }
