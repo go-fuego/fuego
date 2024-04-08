8e137675aa8e5c069020de9f135182f03aca4b65
dylanhitt <dylan.hitt1@gmail.com>
2024-04-08 11:57:52 -0400

    chore: ignore gosec G203 fuego.Markdown

--- views.go
+++ views.go
@@ -11,11 +11,14 @@ import (
 var mdRenderer = html.NewRenderer(html.RendererOptions{Flags: html.CommonFlags | html.SkipHTML})
 
 // Markdown converts a markdown string to HTML.
+// Note: fuego does not protect against malicious content
+// sanitation is up the caller of this funtion.
 func Markdown(content string) template.HTML {
 	if content == "" {
 		return template.HTML("")
 	}
 	mdParser := parser.NewWithExtensions(parser.CommonExtensions | parser.AutoHeadingIDs | parser.NoEmptyLineBeforeBlock | parser.Footnotes | parser.DefinitionLists)
 
+	//nolint:gosec // G203 // the caller of this function needs to sanitize their input
 	return template.HTML(markdown.ToHTML([]byte(content), mdParser, mdRenderer))
 }
