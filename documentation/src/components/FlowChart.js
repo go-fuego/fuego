7126685e0a8761df2d49fad609937659fdc7a631
EwenQuim <ewen.quimerch@gmail.com>
2024-07-08 10:40:57 +0200

    Improved documentation schema and serialization guide

--- documentation/src/components/FlowChart.js
+++ documentation/src/components/FlowChart.js
@@ -3,45 +3,38 @@ import React from "react";
 
 const code = `flowchart TB
 
+Request -- JSON {'a':'value '} --> Deserialization
+
 subgraph fuego
- direction LR
+
+ ErrorHandler{{ErrorHandler}}
 
  subgraph input
-	 direction TB
-	 req(Request) -- JSON {'a':'value '} --> Deserialization
 	 Deserialization -- struct{A:'value '} --> InTransformation
 	 InTransformation -- struct{A:'My Value'} --> Validation
  end
 
+ Validation -- struct{A:'My Value'} --> Controller
+ Controller -- struct{B:'Response!'} --> OutTransformation
+
  subgraph output
-	 direction TB
-	 OutTransformation -- struct{B:'My Response!'} --> resp(Response)
- end
+  OutTransformation -- struct{B:'My Response!'} --> Serialization
+ end 
 
  Controller{{Controller}}
-
- 
- 
- input -- struct{A:'My Value'} --> Controller -- struct{B:'Response'} --> output
- 
- output --> ress(Response)
- 
- input -- error --> ErrorHandler
- Controller -- error --> ErrorHandler
- output -- error --> ErrorHandler
 
  click Controller "/fuego/docs/guides/controllers" "Controllers"
  click Validation "/fuego/docs/guides/validation" "Controllers"
  click InTransformation "/fuego/docs/guides/transformation" "Transformation"
  click OutTransformation "/fuego/docs/guides/transformation" "Transformation"
+ click Serialization "/fuego/docs/guides/serialization" "Serialization"
+ click Deserialization "/fuego/docs/guides/serialization" "Serialization"
  click ErrorHandler "/fuego/docs/guides/errors" "Error Handling"
 end
 
-Request -- JSON {'a':'value '} --> fuego
-fuego -- JSON {'b':'Response!'} --> Response
+Serialization -- JSON{b:'My Response!'} --> resp(Response)
+Controller -. JSON{b:'Custom Response!'} .-> resp(Response)
 
-
-fuego -- JSON {'error':'Error message', 'code': 4xx} --> Response
 
 `;
 
@@ -53,6 +46,11 @@ export function FlowChart({ selected }) {
         `style InTransformation stroke:#f33,stroke-width:4px` +
         "\n" +
         `style OutTransformation stroke:#f33,stroke-width:4px`;
+    } else if (selected === "Serialization") {
+      style +=
+        `style Serialization stroke:#f33,stroke-width:4px` +
+        "\n" +
+        `style Deserialization stroke:#f33,stroke-width:4px`;
     } else {
       style += `style ${selected} stroke:#f33,stroke-width:4px`;
     }
