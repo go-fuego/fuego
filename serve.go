746d45004bf0552ebfef3d722f8b8be685c9bea8
EwenQuim <ewen.quimerch@gmail.com>
2024-11-29 23:49:38 +0100

    Moved params validation to its own file

--- serve.go
+++ serve.go
@@ -54,49 +54,6 @@ func (s *Server) proto() string {
 	return "http"
 }
 
-func validateParams(c ContextNoBody) error {
-	for k, param := range c.params {
-		if param.Default != nil {
-			// skip: param has a default
-			continue
-		}
-
-		if param.Required {
-			switch param.Type {
-			case QueryParamType:
-				if !c.urlValues.Has(k) {
-					err := fmt.Errorf("%s is a required query param", k)
-					return BadRequestError{
-						Title:  "Query Param Not Found",
-						Err:    err,
-						Detail: "cannot parse request parameter: " + err.Error(),
-					}
-				}
-			case HeaderParamType:
-				if !c.HasHeader(k) {
-					err := fmt.Errorf("%s is a required header", k)
-					return BadRequestError{
-						Title:  "Header Not Found",
-						Err:    err,
-						Detail: "cannot parse request parameter: " + err.Error(),
-					}
-				}
-			case CookieParamType:
-				if !c.HasCookie(k) {
-					err := fmt.Errorf("%s is a required cookie", k)
-					return BadRequestError{
-						Title:  "Cookie Not Found",
-						Err:    err,
-						Detail: "cannot parse request parameter: " + err.Error(),
-					}
-				}
-			}
-		}
-	}
-
-	return nil
-}
-
 // initializes any Context type with the base ContextNoBody context.
 //
 //	var ctx ContextWithBody[any] // does not work because it will create a ContextWithBody[any] with a nil value
