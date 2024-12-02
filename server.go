64e58bb07c8976fbcc33b080958da0ebb1241c6c
Ignacio <ignaciovl.j@gmail.com>
2024-12-02 06:25:55 +0000

    set server addres if is empty added again to avoid broke tests and validate if listenner is tls using reflection

--- server.go
+++ server.go
@@ -9,6 +9,7 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"reflect"
 	"time"
 
 	"github.com/getkin/kin-openapi/openapi3"
@@ -394,6 +395,14 @@ func WithListener(listener net.Listener) func(*Server) {
 		s.listener = listener
 		s.Addr = s.listener.Addr().String()
 	}
+}
+
+func isTLSListener(listener net.Listener) bool {
+	listenerType := reflect.TypeOf(listener)
+	if listenerType != nil && listenerType.String() == "*tls.listener" {
+		return true
+	}
+	return false
 }
 
 func WithOpenAPIConfig(openapiConfig OpenAPIConfig) func(*Server) {
