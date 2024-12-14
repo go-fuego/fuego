7f81cfdee839e14356ff35be0ed421643ca638be
dylanhitt <dylan.hitt1@gmail.com>
2024-12-13 22:58:06 -0500

    BREAKING: update WithGlobalResponseType to take

--- openapi_operations_test.go
+++ openapi_operations_test.go
@@ -1,6 +1,7 @@
 package fuego
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -61,32 +62,110 @@ func TestCustomError(t *testing.T) {
 	require.Equal(t, "My Validation Error", *route.Operation.Responses.Map()["400"].Value.Description)
 }
 
-func TestCustomErrorGlobalAndOnRoute(t *testing.T) {
-	type MyGlobalError struct {
+func TestWithGlobalResponseType(t *testing.T) {
+	type MyGlobalResponse struct {
 		Message string
 	}
-	s := NewServer(
-		WithGlobalResponseTypes(400, "My Global Error", MyGlobalError{}),
-		WithGlobalResponseTypes(501, "Another Global Error", MyGlobalError{}),
-	)
-
-	type MyLocalError struct {
+	type MyLocalResponse struct {
 		Message string
 	}
+	t.Run("base", func(t *testing.T) {
+		s := NewServer(
+			WithGlobalResponseTypes(http.StatusNotImplemented, "My Global Error", Response{Type: MyGlobalResponse{}}),
+		)
+		routeGlobal := Get(s, "/test-global", testController)
+		require.Equal(t, "My Global Error", *routeGlobal.Operation.Responses.Value("501").Value.Description)
+	})
 
-	routeGlobal := Get(s, "/test-global", testController)
-	routeCustom := Get(s, "/test-custom", testController,
-		OptionAddError(400, "My Local Error", MyLocalError{}),
-		OptionAddError(419, "My Local Teapot"),
-	)
+	t.Run("base with custom contents", func(t *testing.T) {
+		s := NewServer(
+			WithGlobalResponseTypes(http.StatusNotImplemented, "My Global Error", Response{
+				Type:         MyGlobalResponse{},
+				ContentTypes: []string{"application/x-yaml"},
+			}),
+		)
+		routeGlobal := Get(s, "/test-global", testController)
+		require.NotNil(t, routeGlobal.Operation.Responses.Value("501").Value.Content.Get("application/x-yaml"))
+		require.Nil(t, routeGlobal.Operation.Responses.Value("501").Value.Content.Get("application/xml"))
+	})
 
-	require.Equal(t, "My Global Error", *routeGlobal.Operation.Responses.Value("400").Value.Description, "Overrides Fuego's default 400 error")
-	require.Equal(t, "Another Global Error", *routeGlobal.Operation.Responses.Value("501").Value.Description)
+	t.Run("errors with route overrides", func(t *testing.T) {
+		s := NewServer(
+			WithGlobalResponseTypes(http.StatusBadRequest, "My Global Error", Response{Type: MyGlobalResponse{}}),
+			WithGlobalResponseTypes(http.StatusNotImplemented, "Another Global Error", Response{Type: MyGlobalResponse{}}),
+		)
 
-	require.Equal(t, "My Local Error", *routeCustom.Operation.Responses.Map()["400"].Value.Description, "Local error overrides global error")
-	require.Equal(t, "My Local Teapot", *routeCustom.Operation.Responses.Map()["419"].Value.Description)
-	require.Equal(t, "Internal Server Error _(panics)_", *routeCustom.Operation.Responses.Map()["500"].Value.Description, "Global error set by default by Fuego")
-	require.Equal(t, "Another Global Error", *routeCustom.Operation.Responses.Map()["501"].Value.Description, "Global error is still available")
+		routeGlobal := Get(s, "/test-global", testController)
+		routeCustom := Get(s, "/test-custom", testController,
+			OptionAddResponse(http.StatusBadRequest, "My Local Error", Response{Type: MyLocalResponse{}}),
+			OptionAddError(http.StatusTeapot, "My Local Teapot", Response{Type: HTTPError{}}),
+		)
+
+		require.Equal(t, "My Global Error", *routeGlobal.Operation.Responses.Value("400").Value.Description, "Overrides Fuego's default 400 error")
+		require.Equal(t, "Another Global Error", *routeGlobal.Operation.Responses.Value("501").Value.Description)
+
+		require.Equal(t, "My Local Error", *routeCustom.Operation.Responses.Map()["400"].Value.Description, "Local error overrides global error")
+		require.Equal(t, "My Local Teapot", *routeCustom.Operation.Responses.Map()["418"].Value.Description)
+		require.Equal(t, "Internal Server Error _(panics)_", *routeCustom.Operation.Responses.Map()["500"].Value.Description, "Global error set by default by Fuego")
+		require.Equal(t, "Another Global Error", *routeCustom.Operation.Responses.Map()["501"].Value.Description, "Global error is still available")
+	})
+
+	t.Run("200 responses with overrides", func(t *testing.T) {
+		s := NewServer(
+			WithGlobalResponseTypes(http.StatusCreated, "A Global Response", Response{Type: MyGlobalResponse{}}),
+			WithGlobalResponseTypes(http.StatusAccepted, "My 202 response with content", Response{
+				Type: MyGlobalResponse{}, ContentTypes: []string{"application/x-yaml"},
+			}),
+		)
+
+		t.Run("routeGlobal", func(t *testing.T) {
+			routeGlobal := Get(s, "/test-global", testController)
+			require.Equal(t,
+				"#/components/schemas/ans",
+				routeGlobal.Operation.Responses.Value("200").Value.Content.Get("application/json").Schema.Ref,
+			)
+			require.Equal(t,
+				"#/components/schemas/ans",
+				routeGlobal.Operation.Responses.Value("200").Value.Content.Get("application/xml").Schema.Ref,
+			)
+			require.Equal(t, "A Global Response", *routeGlobal.Operation.Responses.Value("201").Value.Description)
+			require.Equal(t, "My 202 response with content", *routeGlobal.Operation.Responses.Value("202").Value.Description)
+			require.Equal(t,
+				"#/components/schemas/MyGlobalResponse",
+				routeGlobal.Operation.Responses.Value("202").Value.Content.Get("application/x-yaml").Schema.Ref,
+			)
+		})
+
+		t.Run("routeCustom", func(t *testing.T) {
+			routeCustom := Get(s, "/test-custom", testController,
+				OptionAddResponse(http.StatusOK, "My Local Response", Response{Type: MyLocalResponse{}}),
+				OptionAddResponse(http.StatusNoContent, "My No Content", Response{Type: struct{}{}}),
+			)
+			require.Equal(t,
+				"#/components/schemas/MyLocalResponse",
+				routeCustom.Operation.Responses.Value("200").Value.Content.Get("application/json").Schema.Ref,
+			)
+			require.Equal(t,
+				"#/components/schemas/MyLocalResponse",
+				routeCustom.Operation.Responses.Value("200").Value.Content.Get("application/xml").Schema.Ref,
+			)
+			require.Equal(t, "My No Content", *routeCustom.Operation.Responses.Value("204").Value.Description)
+			require.Equal(t, "My 202 response with content", *routeCustom.Operation.Responses.Value("202").Value.Description)
+			require.Equal(t,
+				"#/components/schemas/MyGlobalResponse",
+				routeCustom.Operation.Responses.Value("202").Value.Content.Get("application/x-yaml").Schema.Ref,
+			)
+		})
+	})
+
+	t.Run("should be fatal", func(t *testing.T) {
+		s := NewServer(
+			WithGlobalResponseTypes(http.StatusNotImplemented, "My Global Error", Response{}),
+		)
+		require.Panics(t, func() {
+			Get(s, "/test-global", testController)
+		})
+	})
 }
 
 func TestCookieParams(t *testing.T) {
