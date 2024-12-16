2844870c98f462ce01445d424e7071e49b63a444
rizerkrof <34008166+rizerkrof@users.noreply.github.com>
2024-12-16 23:10:40 +0100

    refactor(cli): change template entity keys

--- cmd/fuego/commands/controller.go
+++ cmd/fuego/commands/controller.go
@@ -18,26 +18,26 @@ func Controller() *cli.Command {
 		Usage:   "creates a new controller file",
 		Aliases: []string{"c"},
 		Action: func(cCtx *cli.Context) error {
-			controllerName := cCtx.Args().First()
+			entityName := cCtx.Args().First()
 
-			if controllerName == "" {
-				controllerName = "newController"
+			if entityName == "" {
+				entityName = "newEntity"
 				fmt.Println("Note: You can add a controller name as an argument. Example: `fuego controller books`")
 			}
 
-			_, err := createController(controllerName)
+			_, err := createController(entityName)
 			if err != nil {
 				return err
 			}
 
-			fmt.Printf("🔥 Controller %s created successfully\n", controllerName)
+			fmt.Printf("🔥 Controller %s created successfully\n", entityName)
 			return nil
 		},
 	}
 }
 
 // createController creates a new controller file
-func createController(controllerName string) (string, error) {
+func createController(entityName string) (string, error) {
 	controllerDir := "./controller/"
 	if _, err := os.Stat(controllerDir); os.IsNotExist(err) {
 		err = os.Mkdir(controllerDir, 0o755)
@@ -54,10 +54,10 @@ func createController(controllerName string) (string, error) {
 	t := language.English
 	titler := cases.Title(t)
 
-	newContent := strings.ReplaceAll(string(templateContent), "newController", controllerName)
-	newContent = strings.ReplaceAll(newContent, "NewController", titler.String(controllerName))
+	newContent := strings.ReplaceAll(string(templateContent), "newEntity", entityName)
+	newContent = strings.ReplaceAll(newContent, "NewEntity", titler.String(entityName))
 
-	controllerPath := fmt.Sprintf("%s%s.go", controllerDir, controllerName)
+	controllerPath := fmt.Sprintf("%s%s.go", controllerDir, entityName)
 
 	err = os.WriteFile(controllerPath, []byte(newContent), 0o644)
 	if err != nil {
