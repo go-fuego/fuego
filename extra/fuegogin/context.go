bc9ec7a3361962022587cb51b155d5652fb88a17
Ewen Quimerc'h <46993939+EwenQuim@users.noreply.github.com>
2024-12-17 13:59:48 +0100

    Update extra/fuegogin/context.go
    Co-authored-by: ccoVeille <3875889+ccoVeille@users.noreply.github.com>

--- extra/fuegogin/context.go
+++ extra/fuegogin/context.go
@@ -25,29 +25,29 @@ type contextWithBody[B any] struct {
 	ginCtx *gin.Context
 }
 
-// Body implements fuego.Ctx.
+// Body satisfies [fuego.Ctx].
 func (c *contextWithBody[B]) Body() (B, error) {
 	var body B
 	err := c.ginCtx.Bind(&body)
 	return body, err
 }
 
-// Context implements fuego.Ctx.
+// Context satisfies [fuego.Ctx].
 func (c *contextWithBody[B]) Context() *gin.Context {
 	return c.ginCtx
 }
 
-// Cookie implements fuego.Ctx.
+// Cookie satisfies [fuego.Ctx].
 func (c *contextWithBody[B]) Cookie(name string) (*http.Cookie, error) {
 	panic("unimplemented")
 }
 
-// Header implements fuego.Ctx.
+// Header satisfies [fuego.Ctx].
 func (c *contextWithBody[B]) Header(key string) string {
 	return c.ginCtx.GetHeader(key)
 }
 
-// MustBody implements fuego.Ctx.
+// MustBody satisfies [fuego.Ctx].
 func (c *contextWithBody[B]) MustBody() B {
 	body, err := c.Body()
 	if err != nil {
@@ -56,77 +56,77 @@ func (c *contextWithBody[B]) MustBody() B {
 	return body
 }
 
-// PathParam implements fuego.Ctx.
+// PathParam satisfies [fuego.Ctx].
 func (c *contextWithBody[B]) PathParam(name string) string {
 	return c.ginCtx.Param(name)
 }
 
-// QueryParam implements fuego.Ctx.
+// QueryParam satisfies [fuego.Ctx].
 func (c *contextWithBody[B]) QueryParam(name string) string {
 	return c.ginCtx.Query(name)
 }
 
-// QueryParamArr implements fuego.Ctx.
+// QueryParamArr satisfies [fuego.Ctx].
 func (c *contextWithBody[B]) QueryParamArr(name string) []string {
 	panic("unimplemented")
 }
 
-// QueryParamBool implements fuego.Ctx.
+// QueryParamBool satisfies [fuego.Ctx].
 func (c *contextWithBody[B]) QueryParamBool(name string) bool {
 	panic("unimplemented")
 }
 
-// QueryParamBoolErr implements fuego.Ctx.
+// QueryParamBoolErr satisfies [fuego.Ctx].
 func (c *contextWithBody[B]) QueryParamBoolErr(name string) (bool, error) {
 	panic("unimplemented")
 }
 
-// QueryParamInt implements fuego.Ctx.
+// QueryParamInt satisfies [fuego.Ctx].
 func (c *contextWithBody[B]) QueryParamInt(name string) int {
 	panic("unimplemented")
 }
 
-// QueryParamIntErr implements fuego.Ctx.
+// QueryParamIntErr satisfies [fuego.Ctx].
 func (c *contextWithBody[B]) QueryParamIntErr(name string) (int, error) {
 	panic("unimplemented")
 }
 
-// QueryParams implements fuego.Ctx.
+// QueryParams satisfies [fuego.Ctx].
 func (c *contextWithBody[B]) QueryParams() url.Values {
 	return c.ginCtx.Request.URL.Query()
 }
 
-// Redirect implements fuego.Ctx.
+// Redirect satisfies [fuego.Ctx].
 func (c *contextWithBody[B]) Redirect(code int, url string) (any, error) {
 	c.ginCtx.Redirect(code, url)
 	return nil, nil
 }
 
-// Render implements fuego.Ctx.
+// Render satisfies [fuego.Ctx].
 func (c *contextWithBody[B]) Render(templateToExecute string, data any, templateGlobsToOverride ...string) (fuego.CtxRenderer, error) {
 	panic("unimplemented")
 }
 
-// Request implements fuego.Ctx.
+// Request satisfies [fuego.Ctx].
 func (c *contextWithBody[B]) Request() *http.Request {
 	return c.ginCtx.Request
 }
 
-// Response implements fuego.Ctx.
+// Response satisfies [fuego.Ctx].
 func (c *contextWithBody[B]) Response() gin.ResponseWriter {
 	return c.ginCtx.Writer
 }
 
-// SetCookie implements fuego.Ctx.
+// SetCookie satisfies [fuego.Ctx].
 func (c *contextWithBody[B]) SetCookie(cookie http.Cookie) {
 }
 
-// SetHeader implements fuego.Ctx.
+// SetHeader satisfies [fuego.Ctx].
 func (c *contextWithBody[B]) SetHeader(key, value string) {
 	c.ginCtx.Header(key, value)
 }
 
-// SetStatus implements fuego.Ctx.
+// SetStatus satisfies [fuego.Ctx].
 func (c *contextWithBody[B]) SetStatus(code int) {
 	c.ginCtx.Status(code)
 }
