b5c9cbae09a2a22c1c4e3510b2360d3c9ea91c3c
dylanhitt <dylan.hitt1@gmail.com>
2024-12-29 12:13:02 -0500

    BREAKING: WithErrorHandler server option to engine option

---
+++ engine_test.go
@@ -0,0 +1,40 @@
+package fuego
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestWithErrorHandler(t *testing.T) {
+	t.Run("default engine", func(t *testing.T) {
+		e := NewEngine()
+		err := NotFoundError{
+			Err: errors.New("Not Found :c"),
+		}
+		errResponse := e.ErrorHandler(err)
+		require.ErrorAs(t, errResponse, &HTTPError{})
+	})
+	t.Run("custom handler", func(t *testing.T) {
+		e := NewEngine(
+			WithErrorHandler(func(err error) error {
+				return fmt.Errorf("%w foobar", err)
+			}),
+		)
+		err := NotFoundError{
+			Err: errors.New("Not Found :c"),
+		}
+		errResponse := e.ErrorHandler(err)
+		require.ErrorAs(t, errResponse, &HTTPError{})
+		require.ErrorContains(t, errResponse, "Not Found :c foobar")
+	})
+	t.Run("should be fatal", func(t *testing.T) {
+		require.Panics(t, func() {
+			NewEngine(
+				WithErrorHandler(nil),
+			)
+		})
+	})
+}
