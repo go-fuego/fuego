ba1b56ab0038e588f3c9857331a90cf73d6f4f38
Evilenzo <lingertell@gmail.com>
2025-04-18 18:25:26 +0300

    add tests

---
+++ extra/fuegogin/adaptor_wrapped_router_test.go
@@ -0,0 +1,119 @@
+package fuegogin
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-fuego/fuego"
+	"github.com/stretchr/testify/assert"
+)
+
+type wrappedRouter struct {
+	router gin.IRouter
+}
+
+func (m *wrappedRouter) Use(handlers ...gin.HandlerFunc) gin.IRoutes {
+	return m.router.Use(handlers...)
+}
+
+func (m *wrappedRouter) Handle(method, path string, handlers ...gin.HandlerFunc) gin.IRoutes {
+	return m.router.Handle(method, path, handlers...)
+}
+
+func (m *wrappedRouter) Any(path string, handlers ...gin.HandlerFunc) gin.IRoutes {
+	return m.router.Any(path, handlers...)
+}
+
+func (m *wrappedRouter) GET(path string, handlers ...gin.HandlerFunc) gin.IRoutes {
+	return m.router.GET(path, handlers...)
+}
+
+func (m *wrappedRouter) POST(path string, handlers ...gin.HandlerFunc) gin.IRoutes {
+	return m.router.POST(path, handlers...)
+}
+
+func (m *wrappedRouter) DELETE(path string, handlers ...gin.HandlerFunc) gin.IRoutes {
+	return m.router.DELETE(path, handlers...)
+}
+
+func (m *wrappedRouter) PATCH(path string, handlers ...gin.HandlerFunc) gin.IRoutes {
+	return m.router.PATCH(path, handlers...)
+}
+
+func (m *wrappedRouter) PUT(path string, handlers ...gin.HandlerFunc) gin.IRoutes {
+	return m.router.PUT(path, handlers...)
+}
+
+func (m *wrappedRouter) OPTIONS(path string, handlers ...gin.HandlerFunc) gin.IRoutes {
+	return m.router.OPTIONS(path, handlers...)
+}
+
+func (m *wrappedRouter) HEAD(path string, handlers ...gin.HandlerFunc) gin.IRoutes {
+	return m.router.HEAD(path, handlers...)
+}
+
+func (m *wrappedRouter) Match(methods []string, path string, handlers ...gin.HandlerFunc) gin.IRoutes {
+	return m.router.Match(methods, path, handlers...)
+}
+
+func (m *wrappedRouter) StaticFile(path, filepath string) gin.IRoutes {
+	return m.router.StaticFile(path, filepath)
+}
+
+func (m *wrappedRouter) StaticFileFS(path, filepath string, fs http.FileSystem) gin.IRoutes {
+	return m.router.StaticFileFS(path, filepath, fs)
+}
+
+func (m *wrappedRouter) Static(path, filepath string) gin.IRoutes {
+	return m.router.Static(path, filepath)
+}
+
+func (m *wrappedRouter) StaticFS(path string, fs http.FileSystem) gin.IRoutes {
+	return m.router.StaticFS(path, fs)
+}
+
+func (m *wrappedRouter) Group(path string, handlers ...gin.HandlerFunc) *gin.RouterGroup {
+	return m.router.Group(path, handlers...)
+}
+
+func (m *wrappedRouter) BasePath() string {
+	if grouped, ok := m.router.(GroupedRouter); ok {
+		return grouped.BasePath()
+	}
+
+	return ""
+}
+
+func dummyHandler(c *gin.Context) {
+	c.AbortWithStatus(http.StatusOK)
+}
+
+func TestWrappedRouterGroup(t *testing.T) {
+	e := fuego.NewEngine()
+	r := gin.New()
+
+	t.Run("basic", func(t *testing.T) {
+		groupV1 := r.Group("/api/v1")
+
+		PostGin(e, &wrappedRouter{router: groupV1}, "/test", dummyHandler)
+
+		assert.NotNil(t, e.OpenAPI.Description().Paths.Value("/api/v1/test"))
+	})
+
+	t.Run("withParam", func(t *testing.T) {
+		groupV1 := r.Group("/api/v1")
+
+		PostGin(e, &wrappedRouter{router: groupV1}, "/test/:id", dummyHandler)
+
+		assert.NotNil(t, e.OpenAPI.Description().Paths.Value("/api/v1/test/{id}"))
+	})
+
+	t.Run("withGroupParam", func(t *testing.T) {
+		groupV1 := r.Group("/api/v1/:id")
+
+		PostGin(e, &wrappedRouter{router: groupV1}, "/test", dummyHandler)
+
+		assert.NotNil(t, e.OpenAPI.Description().Paths.Value("/api/v1/{id}/test"))
+	})
+}
