9014ef9f69a6e1aea34767f7fff0824d85fbb08a
Evilenzo <lingertell@gmail.com>
2025-04-23 20:28:21 +0300

    add more tests & comments

--- extra/fuegogin/adaptor_wrapped_router_test.go
+++ extra/fuegogin/adaptor_wrapped_router_test.go
@@ -90,10 +90,9 @@ func dummyHandler(c *gin.Context) {
 }
 
 func TestWrappedRouterGroup(t *testing.T) {
-	e := fuego.NewEngine()
-	r := gin.New()
-
 	t.Run("basic", func(t *testing.T) {
+		e := fuego.NewEngine()
+		r := gin.New()
 		groupV1 := r.Group("/api/v1")
 
 		PostGin(e, &wrappedRouter{router: groupV1}, "/test", dummyHandler)
@@ -102,6 +101,8 @@ func TestWrappedRouterGroup(t *testing.T) {
 	})
 
 	t.Run("withParam", func(t *testing.T) {
+		e := fuego.NewEngine()
+		r := gin.New()
 		groupV1 := r.Group("/api/v1")
 
 		PostGin(e, &wrappedRouter{router: groupV1}, "/test/:id", dummyHandler)
@@ -110,10 +111,46 @@ func TestWrappedRouterGroup(t *testing.T) {
 	})
 
 	t.Run("withGroupParam", func(t *testing.T) {
+		e := fuego.NewEngine()
+		r := gin.New()
 		groupV1 := r.Group("/api/v1/:id")
 
 		PostGin(e, &wrappedRouter{router: groupV1}, "/test", dummyHandler)
 
 		assert.NotNil(t, e.OpenAPI.Description().Paths.Value("/api/v1/{id}/test"))
+	})
+
+	t.Run("defaultGin", func(t *testing.T) {
+		e := fuego.NewEngine()
+		r := gin.New()
+
+		PostGin(e, r, "/test", dummyHandler)
+
+		// Basic gin engine can be casted to fuegogin.GroupedRouter too
+		// so we expect no extra / in path.
+		assert.Nil(t, e.OpenAPI.Description().Paths.Value("//test"))
+		assert.NotNil(t, e.OpenAPI.Description().Paths.Value("/test"))
+	})
+
+	t.Run("emptyGroup", func(t *testing.T) {
+		e := fuego.NewEngine()
+		r := gin.New()
+		emptyGroup := r.Group("")
+
+		PostGin(e, emptyGroup, "/test", dummyHandler)
+
+		assert.Nil(t, e.OpenAPI.Description().Paths.Value("//test"))
+		assert.NotNil(t, e.OpenAPI.Description().Paths.Value("/test"))
+	})
+
+	t.Run("slashGroup", func(t *testing.T) {
+		e := fuego.NewEngine()
+		r := gin.New()
+		slashGroup := r.Group("/")
+
+		PostGin(e, slashGroup, "/test", dummyHandler)
+
+		assert.Nil(t, e.OpenAPI.Description().Paths.Value("//test"))
+		assert.NotNil(t, e.OpenAPI.Description().Paths.Value("/test"))
 	})
 }
