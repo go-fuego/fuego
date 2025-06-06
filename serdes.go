021877a937772ac3163aef0f09edbfacee163771
Alex Hamilton <alex.hamilton@earthscope.org>
2025-06-06 06:02:05 -0500

    chore: more docstrings for SerDes interface

--- serdes.go
+++ serdes.go
@@ -6,7 +6,17 @@ import (
 )
 
 // SerDes implements serialization and deserialization for a given content type.
+// A round trip of SerDes is expected to be idempotent.
+// This means that the result of Serialize(Deserialize(v)) should be equal to v.
 type SerDes interface {
+
+	// Serialize serializes the value v into a byte slice.
+	// The byte slice is expected to be a valid representation of the value v for the content-type.
+	// If the value v is not valid, an error is returned.
 	Serialize(v any) ([]byte, error)
+
+	// Deserialize deserializes the input into a value of type T.
+	// The input is expected to be a valid representation of the type T for the content-type.
+	// If the input is not valid, an error is returned.
 	Deserialize(ctx context.Context, input io.Reader) (any, error)
 }
