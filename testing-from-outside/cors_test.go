0e57437c352e78102885126e3631416e8eaaec79
jub0bs <jcretel-infosec+github@protonmail.com>
2026-02-16 15:03:12 +0100

    deps: migrate from rs/cors to jub0bs/cors

--- testing-from-outside/cors_test.go
+++ testing-from-outside/cors_test.go
@@ -1,22 +1,28 @@
 package testingfromoutside_test
 
 import (
+	"log"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/rs/cors"
+	"github.com/jub0bs/cors"
 	"github.com/stretchr/testify/require"
 
 	"github.com/go-fuego/fuego"
 )
 
 func TestCors(t *testing.T) {
+	cors, err := cors.NewMiddleware(cors.Config{
+		Origins: []string{"*"},
+		Methods: []string{"GET"},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	s := fuego.NewServer(
 		fuego.WithoutLogger(),
-		fuego.WithGlobalMiddlewares(cors.New(cors.Options{
-			AllowedOrigins: []string{"*"},
-			AllowedMethods: []string{"GET"},
-		}).Handler),
+		fuego.WithGlobalMiddlewares(cors.Wrap),
 	)
 
 	fuego.Get(s, "/", func(c fuego.ContextNoBody) (string, error) {
